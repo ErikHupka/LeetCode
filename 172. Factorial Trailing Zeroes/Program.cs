e9c46135ca166b407a2d980a52747063337d3706
Erik Hupka <erik.hupka1@gmail.com>
2024-06-17 17:38:25 +0200

    172. Factorial Trailing Zeroes

---
+++ 172. Factorial Trailing Zeroes/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Numerics;
+
+Console.WriteLine(TrailingZeroes(n: 6358));
+
+static int TrailingZeroes(int n)
+{
+    int count2 = 0;
+    int count5 = 0;
+
+    for (int i = 2; i <= n; i++)
+    {
+        int num = i;
+        while (num % 2 == 0)
+        {
+            count2++;
+            num /= 2;
+        }
+        while (num % 5 == 0)
+        {
+            count5++;
+            num /= 5;
+        }
+    }
+
+    return Math.Min(count2, count5);
+}
