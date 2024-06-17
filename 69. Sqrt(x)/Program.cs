c1ee67366c2a03f8a6c9e7257670675053242991
Erik Hupka <erik.hupka1@gmail.com>
2024-06-17 18:36:02 +0200

    69. Sqrt(x)

---
+++ 69. Sqrt(x)/Program.cs
@@ -0,0 +1,20 @@
+﻿int input = 15000;
+
+Console.WriteLine(MySqrt(input));
+
+static int MySqrt(int x)
+{
+    if (x < 2)
+        return x;
+
+    double y = x;
+    double z = (y + (x / y)) / 2;
+
+    while (Math.Abs(y - z) >= 0.00001)
+    {
+        y = z;
+        z = (y + (x / y)) / 2;
+    }
+
+    return (int)z;
+}
