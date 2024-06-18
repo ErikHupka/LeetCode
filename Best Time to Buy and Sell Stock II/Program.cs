4e68e92a58f130696c6e93428b4c8688209a2a85
Erik Hupka <erik.hupka1@gmail.com>
2024-06-18 11:40:36 +0200

    Best Time To Buy and Sell Stock II

---
+++ Best Time to Buy and Sell Stock II/Program.cs
@@ -0,0 +1,55 @@
+﻿Console.WriteLine(MaxProfit([1,2,3,4,5]));
+
+static int MaxProfit(int[] prices)
+{
+    int profit = 0;
+    int holdingPrice = 0;
+    bool holding = false;
+
+    for (int i = 0; i < prices.Length; i++)
+    {
+        int todayPrice = prices[i];
+        int tomorrowPrice;
+        if (i < prices.Length - 1)
+        {
+            tomorrowPrice = prices[i + 1];
+        }
+        else
+        {
+            int x = todayPrice - holdingPrice;
+            if (x > 0) { profit += x; }
+            return profit;
+        }
+
+        if (holding == false)
+        {
+            LockPrice(todayPrice);
+
+            if (todayPrice > tomorrowPrice) // Tomorrow will be lower - Continue, Unlock price
+            {
+                AddProfit(todayPrice);
+            }
+        }
+
+        else if (tomorrowPrice > holdingPrice) // Price will be higher tomorrow
+        {
+            AddProfit(todayPrice);
+            LockPrice(todayPrice);
+        }
+    }
+
+    return profit;
+
+
+    void LockPrice(int todayPrice)
+    {
+        holding = true;
+        holdingPrice = todayPrice;
+    }
+
+    void AddProfit(int todayPrice)
+    {
+        holding = false;
+        profit += todayPrice - holdingPrice;
+    }
+}
